3d2e88d766550edcc75b95fad8d164e386dbf5b0
denismnjima <denismnjima@gmail.com>
2024-08-11 14:00:35 +0300

    made the timer functional

--- index.js
+++ index.js
@@ -30,3 +30,22 @@ window.addEventListener('scroll',() => {
     header.classList.remove('header-scrolled')
   }
 })
+
+// timer
+let endDate = new Date("Sep 9, 2024 00:00:00").getTime()
+x=setInterval(() => {
+  let now = new Date().getTime()
+  let timeRemaining = endDate-now
+  let days = Math.floor(timeRemaining/(1000*60*60*24))
+  let hours = Math.floor((timeRemaining%(1000*60*60*24))/(1000*60*60))
+  let minutes = Math.floor(((timeRemaining%(1000*60*60*24))%(1000*60*60))/(1000*60))
+  let seconds = Math.floor((((timeRemaining%(1000*60*60*24))%(1000*60*60)%(1000*60))/1000))
+  if(timeRemaining>0){
+    document.querySelector('#days').innerHTML=days.toString().length==1?`0${days}`:days
+    document.querySelector('#hours').innerHTML=hours.toString().length==1?`0${hours}`:hours
+    document.querySelector('#mins').innerHTML=minutes.toString().length==1?`0${minutes}`:minutes
+    document.querySelector('#secs').innerHTML=seconds.toString().length==1?`0${seconds}`:seconds
+  }
+
+
+},1000)
